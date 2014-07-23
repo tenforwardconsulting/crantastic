305078e12cf854337117682a99dd98a887f1d5c1
Zach Boatrite <zach@tenforwardconsulting.com>
2014-07-23 11:03:30 -0500

    Fix some assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,95 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+$(document).ready(function() {
+
+    prettyPrint();
+
+    $('a[rel*=facebox]').facebox();
+
+    // Try to focus first text input
+    $('input[@type=text]:first').focus();
+
+    // hijacks the pagnation links in /packages
+    $('div.pagination a').livequery('click', function() {
+        var href = this.href;
+
+        // Appends the search term to the URL, to facilitate
+        // browser history and bookmarking
+        if ($("#package-search")[0].value != "" && !href.match(/&search=/)) {
+            href += "&search=" + $("#package-search")[0].value;
+        }
+        location.href = href;
+        return false;
+    });
+
+    $('a#more').livequery('click', function() {
+                              $(this).remove();
+                              $('#spinner').show();
+                              jQuery.get(this.href, {}, function(next) {
+                                             $('#timeline').append(next);
+                                             $('#spinner').hide();
+                                         });
+                              return false;
+                          });
+
+    $('input#package-search').delayedObserver(function(value, object) {
+            $("#packages_list").load('/packages/search',
+                                     { 'search': escape($(this).val()) });
+        $('#spinner').hide();
+    }, 0.5);
+
+    $('#add-tag').click(function() {
+        $('#add-tag-form').toggle().find('#tag_name').focus();
+        return false;
+    });
+
+    $("#login-or-signup").click(function() {
+        jQuery.facebox(
+            '<div class="float-l" style="width: 350px; height: 220px; border: 1px dotted #eee; padding: 9px 9px 9px 18px;">' +
+                '<iframe src="https://crantastic.rpxnow.com/openid/embed?token_url=' +
+                //'http://localhost:3000/session/rpx_token" ' +
+                'http://crantastic.org/session/rpx_token" ' +
+                'scrolling="no" frameBorder="no" style="width:350px;height:220px;"></iframe></div>' +
+                '<div class="float-l" id="login-form" style="width: 320px; height: 220px; margin-left: 45px; padding: 9px 9px 9px 18px; border: 1px dotted #eee"></div>' +
+                '<div style="position: absolute; top: 126px; left: 407px;"><h3 style="color: #aaa">OR</h3></div>',
+            'wide'
+        );
+        $("#login-form").load("/login?show_rpx=false&layout=false");
+        return false;
+    });
+
+});
+
+jQuery(function(){
+    jQuery('.starselect').rating({
+        callback: function(value, link){
+            // Submit the vote
+            $.post(this.form.action, $(this.form).serialize());
+
+            // Display thank you message
+            $(this.form).parent().parent().find(
+                "span.your-rating:first").html("Thank you for your vote!");
+
+            var aspect = $(this.form).find("input[name=aspect]:first")[0].value;
+
+            // Update rating stats
+            $.getJSON(this.form.action + "?aspect=" + aspect, function(data) {
+                      $("span#" + aspect + "-rating").html(
+                          data.average_rating + "/5 (" + data.votes +
+                              (data.votes == 1 ? " vote" :" votes") + ")"
+                      )
+            });
+            return false;
+        }
+    });
+    jQuery('div.rating-cancel').remove();
+});
+
+$(document).ajaxSend(function(event, request, settings) {
+    if (settings.type == 'GET' || settings.type == 'get' || typeof(AUTH_TOKEN) == "undefined") return;
+    // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
+    settings.data = settings.data || "";
+    settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+});
+
+jQuery.ajaxSetup({
+    'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
