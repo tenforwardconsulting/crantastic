02f4038c266555c5d6430c8883bbe63f9e459a6d
Zach Boatrite <zach@tenforwardconsulting.com>
2014-09-29 10:32:23 -0500

    Remove figaro; move rollbar key to initializer

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -3,7 +3,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = 'bd55de465fe14d03ace86915fc7b00a6'
 
   # Here we'll disable in 'test':
   if Rails.env.test?
