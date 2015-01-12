c1e6f78d9b97ef95b387b70ca52e812074984d79
Nonsense331 <brettsamson@gmail.com>
2015-01-12 11:44:34 -0600

    Set rvm ruby version.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -26,7 +26,7 @@ server 'crantastic.org', user: 'crantastic', roles: %w{web app db}
 # Global options
 # --------------
 set :tmp_dir, "/home/crantastic/tmp"
-set :rvm_ruby_string, :release_path
+set :rvm_ruby_version, "ruby-1.9.3-p547@crantastic"
 #  set :ssh_options, {
 #    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
