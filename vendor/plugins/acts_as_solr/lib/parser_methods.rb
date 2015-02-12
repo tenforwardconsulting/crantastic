59f569cfa5833b4433baf5a3c6bb0242ddcb0f4b
Nonsense331 <brettsamson@gmail.com>
2015-02-12 10:01:34 -0600

    Remove extras from solr index while reordering.

--- vendor/plugins/acts_as_solr/lib/parser_methods.rb
+++ vendor/plugins/acts_as_solr/lib/parser_methods.rb
@@ -1,7 +1,7 @@
 module ActsAsSolr #:nodoc:
   module ParserMethods
-    protected    
-    
+    protected
+
     # Method used by mostly all the ClassMethods when doing a search
     def parse_query(query=nil, options={}, models=nil)
       valid_options = [:offset, :limit, :facets, :models, :results_format, :order, :scores, :operator, :include, :lazy]
@@ -15,7 +15,7 @@ module ActsAsSolr #:nodoc:
         query_options[:start] = options[:offset]
         query_options[:rows] = options[:limit]
         query_options[:operator] = options[:operator]
-        
+
         # first steps on the facet parameter processing
         if options[:facets]
           query_options[:facets] = {}
@@ -28,8 +28,8 @@ module ActsAsSolr #:nodoc:
           query_options[:facets][:fields] = options[:facets][:fields].collect{|k| "#{k}_facet"} if options[:facets][:fields]
           query_options[:filter_queries] = replace_types([*options[:facets][:browse]].collect{|k| "#{k.sub!(/ *: */,"_facet:")}"}) if options[:facets][:browse]
           query_options[:facets][:queries] = replace_types(options[:facets][:query].collect{|k| "#{k.sub!(/ *: */,"_t:")}"}) if options[:facets][:query]
-          
-          
+
+
           if options[:facets][:dates]
             query_options[:date_facets] = {}
             # if options[:facets][:dates][:fields] exists then :start, :end, and :gap must be there
@@ -46,7 +46,7 @@ module ActsAsSolr #:nodoc:
                 end
               }
             end
-            
+
             query_options[:date_facets][:start]   = options[:facets][:dates][:start] if options[:facets][:dates][:start]
             query_options[:date_facets][:end]     = options[:facets][:dates][:end] if options[:facets][:dates][:end]
             query_options[:date_facets][:gap]     = options[:facets][:dates][:gap] if options[:facets][:dates][:gap]
@@ -57,10 +57,10 @@ module ActsAsSolr #:nodoc:
               validate_date_facet_other_options(options[:facets][:dates][:other])
               query_options[:date_facets][:other]   = options[:facets][:dates][:other]
             end
-            
-          end          
+
+          end
         end
-        
+
         if models.nil?
           # TODO: use a filter query for type, allowing Solr to cache it individually
           models = "AND #{solr_type_condition}"
@@ -68,56 +68,56 @@ module ActsAsSolr #:nodoc:
         else
           field_list = "id"
         end
-        
+
         query_options[:field_list] = [field_list, 'score']
         query = "(#{query.gsub(/ *: */,"_t:")}) #{models}"
-        order = options[:order].split(/\s*,\s*/).collect{|e| e.gsub(/\s+/,'_t ').gsub(/\bscore_t\b/, 'score')  }.join(',') if options[:order] 
-        query_options[:query] = replace_types([query])[0] # TODO adjust replace_types to work with String or Array  
+        order = options[:order].split(/\s*,\s*/).collect{|e| e.gsub(/\s+/,'_t ').gsub(/\bscore_t\b/, 'score')  }.join(',') if options[:order]
+        query_options[:query] = replace_types([query])[0] # TODO adjust replace_types to work with String or Array
 
         if options[:order]
           # TODO: set the sort parameter instead of the old ;order. style.
           query_options[:query] << ';' << replace_types([order], false)[0]
         end
-        
+
         ActsAsSolr::Post.execute(Solr::Request::Standard.new(query_options))
       rescue
         raise "There was a problem executing your search: #{$!} in #{$!.backtrace.first}"
-      end            
+      end
     end
-    
+
     def solr_type_condition
       subclasses.inject("(#{solr_configuration[:type_field]}:#{self.name}") do |condition, subclass|
         condition << " OR #{solr_configuration[:type_field]}:#{subclass.name}"
       end << ')'
     end
-    
+
     # Parses the data returned from Solr
     def parse_results(solr_data, options = {})
       results = {
         :docs => [],
         :total => 0
       }
-      
+
       configuration = {
         :format => :objects
       }
       results.update(:facets => {'facet_fields' => []}) if options[:facets]
       return SearchResults.new(results) if (solr_data.nil? || solr_data.total_hits == 0)
-      
+
       configuration.update(options) if options.is_a?(Hash)
 
       ids = solr_data.hits.collect {|doc| doc["#{solr_configuration[:primary_key_field]}"]}.flatten
-      
+
       result = find_objects(ids, options, configuration)
-      
+
       add_scores(result, solr_data) if configuration[:format] == :objects && options[:scores]
-      
+
       results.update(:facets => solr_data.data['facet_counts']) if options[:facets]
       results.update({:docs => result, :total => solr_data.total_hits, :max_score => solr_data.max_score, :query_time => solr_data.data['responseHeader']['QTime']})
       SearchResults.new(results)
     end
-    
-    
+
+
     def find_objects(ids, options, configuration)
       result = if configuration[:lazy] && configuration[:format] != :ids
         ids.collect {|id| ActsAsSolr::LazyDocument.new(id, self)}
@@ -129,14 +129,20 @@ module ActsAsSolr #:nodoc:
       else
         ids
       end
-        
+
       result
     end
-    
+
     # Reorders the instances keeping the order returned from Solr
     def reorder(things, ids)
       ordered_things = Array.new(things.size)
-      raise "Out of sync! Found #{ids.size} items in index, but only #{things.size} were found in database!" unless things.size == ids.size
+
+      unless things.size == ids.size
+        (ids - (things.collect{|x| x.id})).collect{|missing| ids[ids.index(missing)] = nil}
+        ids = ids.compact
+      end
+
+      raise "Out of sync! Found #{ids.size} items in index, but only #{things.size} were found in database! Remove #{(ids - (things.collect{|x| x.id})).to_sentence}." unless things.size == ids.size
       things.each do |thing|
         position = ids.index(thing.id)
         ordered_things[position] = thing
@@ -166,20 +172,20 @@ module ActsAsSolr #:nodoc:
       end
       strings
     end
-    
+
     # Adds the score to each one of the instances found
     def add_scores(results, solr_data)
       with_score = []
       solr_data.hits.each do |doc|
-        with_score.push([doc["score"], 
+        with_score.push([doc["score"],
           results.find {|record| scorable_record?(record, doc) }])
       end
-      with_score.each do |score, object| 
+      with_score.each do |score, object|
         class << object; attr_accessor :solr_score; end
         object.solr_score = score
       end
     end
-    
+
     def scorable_record?(record, doc)
       doc_id = doc["#{solr_configuration[:primary_key_field]}"]
       if doc_id.nil?
@@ -189,13 +195,13 @@ module ActsAsSolr #:nodoc:
         record_id(record).to_s == doc_id.to_s
       end
     end
-    
+
     def validate_date_facet_other_options(options)
       valid_other_options = [:after, :all, :before, :between, :none]
       options = [options] unless options.kind_of? Array
       bad_options = options.map {|x| x.to_sym} - valid_other_options
       raise "Invalid option#{'s' if bad_options.size > 1} for faceted date's other param: #{bad_options.join(', ')}. May only be one of :after, :all, :before, :between, :none" if bad_options.size > 0
     end
-    
+
   end
 end
