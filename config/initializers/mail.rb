c170ba17153aa7d97b73bf56ce17ab4b58582f4e
Nonsense331 <brettsamson@gmail.com>
2015-03-16 09:27:14 -0500

    Move smtp settings back to initializer.

---
+++ config/initializers/mail.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'crantastic.org',
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :user_name => 'cranatic',
+    :password => ENV['GMAIL']
+  }
+end
